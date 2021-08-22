d6ac22e9c87a26680d44acd445d5751369d3bd3a
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2021-08-22 15:57:31 +0530

    Update WeatherData.js

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,120 +1,98 @@
-import React, { useEffect } from "react";
+// //39984a5b5194233335e567927c183d00
+// api.openweathermap.org/data/2.5/weather?q=delhi&appid=39984a5b5194233335e567927c183d00
+//pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={API key}
 
-const WeatherData = ({
-  temp,
-  humidity,
-  pressure,
-  weathermood,
-  name,
-  speed,
-  country,
-  sunset,
-}) => {
-  const [weatherState, setWeatheState] = React.useState("");
 
-  useEffect(() => {
-    if (weathermood) {
-      switch (weathermood) {
-        case "Clouds":
-          setWeatheState("wi-day-cloudy");
-          break;
-        case "Haze":
-          setWeatheState("wi-fog");
-          break;
-        case "Clear":
-          setWeatheState("wi-day-sunny");
-          break;
-        case "Mist":
-          setWeatheState("wi-dust");
-          break;
-          case "Rain":
-            setWeatheState("wi-dust");
-            break;
 
-        default:
-          setWeatheState("wi-day-rain");
-          break;
-      }
-    }
-  }, [weathermood]);
+import React,{ useState, useEffect} from "react";
+import "./style.css";
+import WeatherData from "./WeatherData";
+const Temp = () => {
+    const [searchValue, setSearchValue] = useState("deoria");
+    const [tempInfo, setTempInfo] = useState({});
 
-  // converting the seconds into time
-  let sec = sunset;
-  let date = new Date(sec * 1000);
-  let timeStr = `${date.getHours()}:${date.getMinutes()}`;
-  return (
-    <>
-      <article className="widget">
-        <div className="weatherIcon">
-          <i className={`wi ${weatherState}`}></i>
-          <i className={`wi ${weatherState}`}></i>
-          <i className={`wi ${weatherState}`}></i>
-        </div>
-        
-        <div className="weatherInfo">
-          <div className="temperature">
-            <span>{temp}&deg;</span>
-          </div>
+    // const getWeatherForecast= async () => {
+    //   try {
+    //     let url = `http://pro.openweathermap.org/data/2.5/forecast/hourly?q=deoria&appid=67bb5a7ee9338a79307ff0dec048f01d`
+    //     let  result =await fetch(url);
+    //     let data = await result.json();
+    //     console.log(result);
 
-          <div className="description">
-            <div className="weatherCondition">{weathermood}</div>
-            <div className="place">
-              {name}, {country}
-            </div>
-          </div>
-        </div>
+    //   }catch (error) {
+    //     console.log(error);
+    //   }
+    // }
 
-        <div className="date"> {new Date().toLocaleString()} </div>
-      
-        {/* our 4column section  */}
-        <div className="extra-temp">
-          <div className="temp-info-minmax">
-            <div className="two-sided-section">
-              <p>
-                <i className={"wi wi-sunset"}></i>
-              </p>
-              <p className="extra-info-leftside">
-                {timeStr} PM <br />
-                Sunset
-              </p>
-            </div>
+const getWeatherInfo = async () => {
+       try {
+           let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=d47e68282317548f4eaf68d2bd69ded6`
 
-            <div className="two-sided-section">
-              <p>
-                <i className={"wi wi-humidity"}></i>
-              </p>
-              <p className="extra-info-leftside">
-                {humidity} <br />
-                Humidity
-              </p>
-            </div>
-          </div>
-
-          <div className="weather-extra-info">
-            <div className="two-sided-section">
-              <p>
-                <i className={"wi wi-rain"}></i>
-              </p>
-              <p className="extra-info-leftside">
-                {pressure} <br />
-                Pressure
-              </p>
-            </div>
-
-            <div className="two-sided-section">
-              <p>
-                <i className={"wi wi-strong-wind"}></i>
-              </p>
-              <p className="extra-info-leftside">
-                {speed} <br />
-                Speed
-              </p>
-            </div>
-          </div>
-        </div>
-      </article>
-    </>
-  );
-};
-
-export default WeatherData;
+           let res = await fetch(url);
+           let data = await res.json();
+     
+           const { temp, humidity, pressure } = data.main;
+           const { main: weathermood } = data.weather[0];
+           const { name } = data;
+           const { speed } = data.wind;
+           const { country, sunset } = data.sys;
+     
+           const myNewWeatherInfo = {
+             temp,
+             humidity,
+             pressure,
+             weathermood,
+             name,
+             speed,
+             country,
+             sunset,
+           };
+     
+           setTempInfo(myNewWeatherInfo);
+         } catch (error) {
+           console.log(error);
+         }
+       };
+     
+       useEffect(() => {
+         getWeatherInfo();
+       },[]);
+     
+       return (
+         <div className="main">
+           <div className="wrap">
+             <div className="search">
+               <input
+                 type="search"
+                 placeholder="search..."
+                 autoFocus
+                 id="search"
+                 className="searchTerm"
+                 value={searchValue}
+                 onChange={(e) => setSearchValue(e.target.value)}
+               />
+     
+               <button
+                 className="searchButton"
+                 type="button"
+                 onClick={getWeatherInfo}>
+                 Search
+               </button>
+             </div>
+           </div>
+     
+           {/* our temp card  */}
+           <WeatherData {...tempInfo} />
+         
+            <div className="extra"></div>
+            <div className="extra"></div>
+            <div className="extra"></div>
+            <div className="extra"></div>
+            <div className="extra"></div>
+            <div className="extra"></div>
+            <div className="extra"></div>
+          
+         </div>
+       );
+     };
+     
+     export default Temp;
