b3173bf30fcc18829d266423933e9cbbf65d8200
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2021-08-22 16:08:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react"
-// import "./App.css";
 import Temp from "./Temp"
 
 
 function App() {
-
-   
   return (
     <div className="App">
       <Temp/>
-
     </div>
   );
 }
